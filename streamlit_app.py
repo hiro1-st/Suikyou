01ed35171781a965a8e94c47e75f1f62cce5e6e0
hiro1-st <haohuanghouteng@gmail.com>
2025-06-12 08:52:39 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,29 +11,6 @@ st.header("方法1: カスタムCSS + HTML")
 # CSSスタイルを定義
 st.markdown("""
 <style>
-    .small-button {
-        background-color: #ff6b6b;
-        color: white;
-        padding: 5px 10px;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        font-size: 12px;
-        width: 100px;
-        height: 30px;
-    }
-    
-    .medium-button {
-        background-color: #4ecdc4;
-        color: white;
-        padding: 10px 20px;
-        border: none;
-        border-radius: 10px;
-        cursor: pointer;
-        font-size: 16px;
-        width: 200px;
-        height: 50px;
-    }
     
     .large-button {
         background-color: #45b7d1;
@@ -58,11 +35,6 @@ st.markdown("""
 # HTMLボタンを作成
 col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.markdown('<button class="small-button custom-button">小さいボタン</button>', unsafe_allow_html=True)
-
-with col2:
-    st.markdown('<button class="medium-button custom-button">中くらいボタン</button>', unsafe_allow_html=True)
 
 with col3:
     st.markdown('<button class="large-button custom-button">大きいボタン</button>', unsafe_allow_html=True)
