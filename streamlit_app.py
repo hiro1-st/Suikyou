584c83fdd013914914cab3466683926c7a1015bc
hiro1-st <haohuanghouteng@gmail.com>
2025-07-04 14:26:15 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,17 @@ st.title("Streamlit ボタンのデモ")
 # 4つのボタンを2つずつ縦並びで表示
 col1, col2 = st.columns(2)
 
-st.link_button("ページ１に移動する", "https://suikyou-hzyvkrtvfpqe4dfufv9xym.streamlit.app/page1")
+import tkinter as tk
+import webbrowser
+
+def open_page():
+    webbrowser.open("https://suikyou-hzyvkrtvfpqe4dfufv9xym.streamlit.app/page1", new=0)
+
+root = tk.Tk()
+button = tk.Button(root, text="ページ1", command=open_page)
+button.pack()
+root.mainloop()
+
 
 with col1:
     if st.button("ボタン1", key="btn1"):
