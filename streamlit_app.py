f92fefba9523d001177db1a1b0cd2036f25c579c
hiro1-st <haohuanghouteng@gmail.com>
2025-07-04 14:27:21 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import tkinter as tk
+import webbrowser
 
 # アプリのタイトル
 st.title("Streamlit ボタンのデモ")
@@ -6,8 +8,6 @@ st.title("Streamlit ボタンのデモ")
 # 4つのボタンを2つずつ縦並びで表示
 col1, col2 = st.columns(2)
 
-import tkinter as tk
-import webbrowser
 
 def open_page():
     webbrowser.open("https://suikyou-hzyvkrtvfpqe4dfufv9xym.streamlit.app/page1", new=0)
